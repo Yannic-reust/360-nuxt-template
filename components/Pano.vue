cdcfa5ba157e1f488730d8345fa1f068a7609d63
Yannic-reust <62382435+Yannic-reust@users.noreply.github.com>
2023-10-11 15:03:53 +0200

    working

---
+++ components/Pano.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="coucou" class="panorama-viewer"></div>
+</template>
+
+<script lang="ts">
+    import * as PANOLENS from "panolens";
+export default {
+  name: "opening-Hours",
+
+  data() {
+    return {};
+  },
+  methods: {},
+
+  mounted() {
+    const panorama = new PANOLENS.ImagePanorama(
+      "https://melodious-cranachan-68635f.netlify.app/test/angra.jpg"
+    );
+
+    const viewer = new PANOLENS.Viewer({
+      container: document.querySelector("#coucou"),
+    });
+
+    viewer.add(panorama);
+  },
+};
+</script>
+
+<style>
+#coucou{
+    height:600px;
+}
+</style>
